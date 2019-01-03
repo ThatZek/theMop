a9d5612d59f16aa5c6f66a50121e0ae52577a08e
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2019-01-02 20:08:20 -0700

    remaking find.js

--- cmds/RaidLead/find.js
+++ cmds/RaidLead/find.js
@@ -1,31 +1,9 @@
 const Discord = require("discord.js");
 const config = require("../../config.json");
 module.exports.run = async (client, msg, args) => {
-    if (args[0] === null) return msg.reply('You need to specify a user!')
-    let member = msg.guild.members.find('displayName', args[0])
-    if (member === null) {
-        let arg = ',' + args[0]
-        member = msg.guild.members.find('displayName', arg)
-        if (member === null) {
-            arg = "'" + args[0]
-            member = msg.guild.members.find('displayName', arg)
-            if (member === null) {
-                arg = "#" + args[0]
-                member = msg.guild.members.find('displayName', arg)
-                if (member === null) {
-                    arg = "!" + args[0]
-                    member = msg.guild.members.find('displayName', arg)
-                    if (member === null) return msg.reply('I could not find the user!')
-                }
-            }
-        }
-    }
-    channel = msg.channel
-    client.channels.get(channel).send({
-        embed: {
-            title: 'User Found!',
-            description: '<@' + member.user.id + '>'
-        }
+    
+    client.msg.members.map(member => {
+        
     })
 }
 module.exports.help = {
