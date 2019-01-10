4d1d709ee956982e82fc73ac5ef054ef48bbbc2c
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2019-01-10 13:11:21 -0700

    Update sayembed.js

--- cmds/Admin/sayembed.js
+++ cmds/Admin/sayembed.js
@@ -3,13 +3,13 @@ const config = require("../../config.json");
 
 module.exports.run = async (client, msg, args) => {
     
-    //const body = msg;
-    //msg.channel.send({
-        //embed: {
-            //description: body
-         //}
-    //});
-
+    const body = msg;
+    msg.channel.send({
+        embed: {
+            description: body
+         }
+    });
+}
 module.exports.help = {
     name: 'saye',
     role: config.modrole,
