e159e5264444c70d2f23b7c59091822a57c04411
Zakhersas <11006634+Zakhersas@users.noreply.github.com>
2019-01-10 09:53:12 -0700

    Update mverify.js

--- cmds/Admin/mverify.js
+++ cmds/Admin/mverify.js
@@ -1,4 +1,17 @@
-const Discord = require('discord.js')
-const config = require('../../config.json')
-
-module.exports.run = async (
+const Discord = require("discord.js");
+const config = require("../../config.json");
+module.exports.run = async (client, msg, args) => {
+    const user = msg.mentions.users.first();
+    const member = msg.mentions.members.first();
+        args.shift()
+        const username = args[0];
+        member.setnickname(username);
+ 
+}
+module.exports.help = {
+    name: 'mverify',
+    role: config.modrole,
+    usage: '`user` `Realm Name`',
+    desc: `manually verifies mentioned user`,
+    example: '@That "One" Turtle#1123 Zekhersas'
+}
